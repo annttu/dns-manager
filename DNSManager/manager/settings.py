aef764b00159f08e0609ce1e24d2bff2b463b2ef
Antti Jaakkola <annttu@git.annttu.fi>
2015-03-08 21:53:27 +0200

    use Forwarded IP

--- DNSManager/manager/settings.py
+++ DNSManager/manager/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'XForwardedForMiddleware.XForwardedForMiddleware',
 )
 
 ROOT_URLCONF = 'manager.urls'
