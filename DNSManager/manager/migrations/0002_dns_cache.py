7450ddc6fc825b2089191b8634bd8ba64e7e7881
Antti Jaakkola <annttu@git.annttu.fi>
2015-05-04 10:07:41 +0300

    fixed migration

--- DNSManager/manager/migrations/0002_dns_cache.py
+++ DNSManager/manager/migrations/0002_dns_cache.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('manager', '0001_initial'),
     ]
 
     operations = [
